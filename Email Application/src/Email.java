8457631384a75ad375b6f9f006b308d900babfb2
Conor <Conor@cmcerlean.co.uk>
2019-01-04 20:24:44 +0000

    Beginning creation of Email Class

---
+++ Email Application/src/Email.java
@@ -0,0 +1,24 @@
+
+public class Email {
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive first name and last name.
+	public Email(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	// Ask for the department
+	
+	// Generate a random password
+	
+	// Set mailbox capacity
+	
+	// Set alternate email
+	
+	// Change the password
+}
---
+++ Email Application/src/EmailApp.java
@@ -0,0 +1,21 @@
+
+public class EmailApp {
+/**In this project I'm playing the role of IT support tasked with creating 
+ * email accounts for new hires.	
+ */
+	
+/**Goals of this project are as follows:
+ * The application should:
+ *		*Generate email with syntax: firstname.lastname@department.company.com
+ *		*Determine the department & if none leave blank
+ *		*Generate a random string for the password
+ *		*Have set methods to change the password, set the mailbox capacity, and define and alternate email address.
+ *		*Have get methods to display the name, email and mailbox capacity.
+ * 	
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
